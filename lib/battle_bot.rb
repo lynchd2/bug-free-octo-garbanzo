90c5f3751ae1e3319fa8a3334d20a3240d0ffe48
Dylan Lynch <lynchd2@tcnj.edu>
2016-07-17 16:46:32 -0400

    All but two specs will not pass

--- lib/battle_bot.rb
+++ lib/battle_bot.rb
@@ -1,31 +1,28 @@
+require_relative 'weapon'
+
 class BattleBot
+
+  attr_reader :name, :weapon, :health, :enemies
   @@count = 0
-  attr_reader :name, :weapon, :health, :count
- 
 
   def initialize(name, weapon = nil)
     @name = name
     @health = 100
     @weapon = weapon
-    @@count += 1
     @enemies = []
+    @@count += 1
   end
 
-  def enemies
-    all_enemies = []
-    return []
+  def self.count
+    @@count
   end
 
   def dead?
-    if self.health <= 0
-      return true
-    else
-      return false
-    end
+    self.health <= 0
   end
 
   def has_weapon?
-    return @weapon != nil
+    self.weapon != nil
   end
 
   def pick_up(new_weapon)
@@ -33,55 +30,52 @@ class BattleBot
     raise ArgumentError.new("") if new_weapon.bot != nil
     if @weapon == nil
        @weapon = new_weapon 
-     end
+       @weapon.bot = self
+       @weapon
+    end
   end
 
   def drop_weapon
-    self.weapon = nil
+    @weapon.bot = nil
+    @weapon = nil
   end
 
   def take_damage(damage)
-    raise ArgumentError.new("") if damage.class == String
-    self.health -= damage
-    @@count -= 1 if self.health < 1
-    return @health
+    raise ArgumentError.new("") if !damage.is_a?(Fixnum)
+    @health -= damage
+    @health = 0 if @health <= 0
+    @@count -= 1 if @health <= 0
+    @health
   end
 
   def heal
-    self.health += 10 unless self.health > 99 || self.health < 1
+    @health += 10 unless @health > 99 || @health < 1
   end
 
-  def attack(bot)
-    raise ArgumentError.new("") if bot.class != BattleBot
-    raise ArgumentError.new("") if bot == self
-    raise ArgumentError.new("") if bot.weapon == nil
-    bot.receive_attack_from(self)
+  def attack(enemy)
+    raise ArgumentError.new("") if !enemy.is_a?(BattleBot) || enemy == self || !!enemy.weapon 
+    enemy.receive_attack_from(self)
   end
 
   def receive_attack_from(enemy)
-    if self.enemies.include?(enemy)
-      self.enemies << enemy 
+    if !enemy.is_a?(BattleBot) || enemy.weapon.nil?  || enemy == self
+      raise ArgumentError.new("")
+    else
+      if !enemy.dead? && !self.dead? && enemy.has_weapon?
+        @enemies << enemy if !@enemies.include?(enemy)
+        take_damage(enemy.weapon.damage)
+        defend_against(enemy)
+      end
     end
-    take_damage(enemy.weapon.damage)
-    self.defend_against(enemy)
   end
 
   def defend_against(bot)
-    if !dead? && has_weapon?
-      self.attack(bot)
+    if !(bot.is_a?(BattleBot) || bot == self)
+      raise ArgumentError.new("")
+    else
+      if !dead? && !bot.dead? && has_weapon?
+        attack(bot)
+      end
     end
   end
-
-  private
-
-  def health=(health)
-    @health = health
-  end
-
-  def weapon=(weapon)
-    @weapon = weapon
-  end
-
-
-
 end
--- lib/weapon.rb
+++ lib/weapon.rb
@@ -1,23 +1,31 @@
 class Weapon
-  attr_reader :name, :damage
-  attr_accessor :bot
+  attr_reader :name, :damage, :bot
 
-  def initialize(name, damage = 0, bot = nil)
+  def initialize(name, damage = 0)
     raise ArgumentError.new("") if name.class != String
     raise ArgumentError.new("") if damage.class != Fixnum
     @name = name
     @damage = damage
-    raise ArgumentError.new("") if bot != nil
-    @bot = bot
+    @bot = nil
 
     
   end
 
   def picked_up?
-    
+    !!@bot
   end
 
-  
+  def bot=(bot)
+    if !bot.is_a?(BattleBot)
+      if bot != nil
+        raise ArgumentError.new("")
+      else
+        bot = nil
+      end
+    else
+      @bot = bot
+    end
+  end
 
 
 
